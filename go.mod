7dd2941bcd63cdb53c51fb0528388ad05ffddf02
Steven Allen <steven@stebalien.com>
2019-03-21 22:49:44 -0700

    cluster pinner

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/Stebalien/cluster-pin
+
+go 1.12
+
+require (
+	github.com/adrg/xdg v0.0.0-20190319220657-88e5137d2444
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/hsanjuan/go-libp2p-http v0.0.2 // indirect
+	github.com/ipfs/go-cid v0.0.1
+	github.com/ipfs/go-ipfs-api v0.0.1 // indirect
+	github.com/ipfs/go-ipfs-files v0.0.2 // indirect
+	github.com/ipfs/go-ipfs-http-client v0.0.1
+	github.com/ipfs/go-path v0.0.3 // indirect
+	github.com/ipfs/interface-go-ipfs-core v0.0.1
+	github.com/ipfs/ipfs-cluster v0.10.0
+	github.com/libp2p/go-libp2p-host v0.0.2 // indirect
+	github.com/libp2p/go-libp2p-peer v0.1.0
+	github.com/libp2p/go-libp2p-peerstore v0.0.1
+	github.com/libp2p/go-libp2p-pnet v0.0.1 // indirect
+	github.com/multiformats/go-multiaddr v0.0.2
+	go.opencensus.io v0.19.2 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+
+	xdg "github.com/adrg/xdg"
+	cid "github.com/ipfs/go-cid"
+	ipfshttp "github.com/ipfs/go-ipfs-http-client"
+	ipfsapi "github.com/ipfs/interface-go-ipfs-core"
+	clusterapi "github.com/ipfs/ipfs-cluster/api"
+	clusterhttp "github.com/ipfs/ipfs-cluster/api/rest/client"
+	peer "github.com/libp2p/go-libp2p-peer"
+	pstore "github.com/libp2p/go-libp2p-peerstore"
+)
+
+func main() {
+	err := run(os.Args[1:])
+	if err == nil {
+		return
+	}
+	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	os.Exit(1)
+}
+
+func clusterClient() (clusterhttp.Client, error) {
+	type decodeConfig struct {
+		clusterhttp.Config
+		APIAddr, ProxyAddr clusterapi.Multiaddr
+	}
+	configFilePath, err := xdg.SearchConfigFile("ipfs-cluster/client.json")
+	if err != nil {
+		return nil, err
+	}
+
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	var config decodeConfig
+	if err := json.NewDecoder(configFile).Decode(&config); err != nil {
+		return nil, err
+	}
+	config.Config.APIAddr = config.APIAddr.Multiaddr
+	config.Config.ProxyAddr = config.ProxyAddr.Multiaddr
+
+	return clusterhttp.NewDefaultClient(&config.Config)
+}
+
+func connect(ctx context.Context, ipfs ipfsapi.CoreAPI, cluster clusterhttp.Client) error {
+	clusterPeers, err := cluster.Peers(ctx)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	pinfos := make(map[peer.ID]*pstore.PeerInfo, len(clusterPeers))
+	for _, p := range clusterPeers {
+		for _, addr := range p.IPFS.Addresses {
+			pii, err := pstore.InfoFromP2pAddr(addr.Multiaddr)
+			if err != nil {
+				return err
+			}
+			pi, ok := pinfos[pii.ID]
+			if !ok {
+				pi = &pstore.PeerInfo{ID: pii.ID}
+				pinfos[pi.ID] = pi
+			}
+			pi.Addrs = append(pi.Addrs, pii.Addrs...)
+		}
+	}
+
+	wg.Add(len(pinfos))
+	for _, pi := range pinfos {
+		go func(pi *pstore.PeerInfo) {
+			err := ipfs.Swarm().Connect(ctx, *pi)
+			if err != nil {
+				log.Printf("failed to connect to %s: %s", pi.ID, err)
+			}
+		}(pi)
+	}
+	wg.Wait()
+	return nil
+}
+
+func pin(ctx context.Context, cluster clusterhttp.Client, hash, name string) error {
+	object, err := cid.Decode(hash)
+	if err != nil {
+		return fmt.Errorf("could not decode cid")
+	}
+	return cluster.Pin(ctx, object, clusterapi.PinOptions{
+		Name: name,
+	})
+}
+
+type pinJob struct {
+	name, hash string
+}
+
+type pinResult struct {
+	pinJob
+	error
+}
+
+func run(args []string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	cluster, err := clusterClient()
+	if err != nil {
+		return err
+	}
+
+	ipfs, err := ipfshttp.NewLocalApi()
+	if err != nil {
+		return err
+	}
+
+	// Connect in the background.
+	go func() {
+		err := connect(ctx, ipfs, cluster)
+		if err != nil {
+			log.Printf("failed to connect to cluster: %s", err)
+		}
+	}()
+
+	jobs := make(chan pinJob)
+	// read in jobs
+	go func() {
+		defer close(jobs)
+		if len(args) == 0 {
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				pieces := strings.SplitN(scanner.Text(), " ", 2)
+				hash := pieces[0]
+				name := ""
+				if len(pieces) == 2 {
+					name = pieces[1]
+				}
+				select {
+				case jobs <- pinJob{name: name, hash: hash}:
+				case <-ctx.Done():
+					return
+				}
+			}
+		} else {
+			hash := args[0]
+			name := strings.Join(args[1:], " ")
+			select {
+			case jobs <- pinJob{name: name, hash: hash}:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	// process jobs
+	results := make(chan pinResult)
+	var wg sync.WaitGroup
+	for i := 0; i < 16; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for job := range jobs {
+				select {
+				case results <- pinResult{job, pin(ctx, cluster, job.hash, job.name)}:
+				case <-ctx.Done():
+				}
+			}
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// print results
+	failed := false
+	for r := range results {
+		if r.error == nil {
+			fmt.Printf("pinned: %s\t%s\n", r.hash, r.name)
+		} else {
+			failed = true
+			fmt.Printf("failed: %s\t%s\n", r.hash, r.error)
+		}
+	}
+
+	if failed {
+		return fmt.Errorf("failed to pin everything")
+	}
+	return nil
+}
